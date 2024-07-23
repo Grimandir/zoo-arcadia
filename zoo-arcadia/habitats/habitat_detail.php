8387c2d71452f3d77b2179e1b29a4cbd8babeaf9
Grimandir <tatan.nantais22@hotmail.fr>
2024-07-23 11:40:04 +0200

    Ajouter les fichiers PHP, HTML et CSS pour le module Zoo Arcadia

--- zoo-arcadia/habitats/habitat_detail.php
+++ zoo-arcadia/habitats/habitat_detail.php
@@ -1,5 +1,5 @@
 <?php
-require 'config.php'; // Inclure la configuration de la base de données
+require '../config.php'; // Inclure la configuration de la base de données
 
 // Vérifier si l'ID de l'habitat est passé en paramètre GET
 if (!isset($_GET['habitat_id']) || empty($_GET['habitat_id'])) {
@@ -30,8 +30,8 @@ $animals = $stmt->fetchAll();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?= htmlspecialchars($habitat['name']) ?> - Détails</title>
-    <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 
 <body>
@@ -43,7 +43,7 @@ $animals = $stmt->fetchAll();
             <?php foreach ($animals as $animal) : ?>
                 <div class="col-md-4">
                     <div class="card img-hover-zoom">
-                        <img src="assets/images/<?= htmlspecialchars($animal['image']) ?>" class="card-img-top" alt="<?= htmlspecialchars($animal['name']) ?>">
+                        <img src="../assets/images/<?= htmlspecialchars($animal['image']) ?>" class="card-img-top" alt="<?= htmlspecialchars($animal['name']) ?>">
                         <div class="card-body">
                             <h5 class="card-title"><?= htmlspecialchars($animal['name']) ?></h5>
                             <p class="card-text"><?= htmlspecialchars($animal['species']) ?></p>
@@ -56,7 +56,9 @@ $animals = $stmt->fetchAll();
         </div>
     </div>
     <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
-    <script src="assets/js/script.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="../assets/js/script.js"></script>
 </body>
 
 </html>
